2053264db322e9b4324b3e4c0396e4a713d13c86
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-04-06 09:46:13 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,4 @@
+// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
@@ -7,7 +8,7 @@ const firebaseConfig = {
   authDomain: "github-9d1dc.firebaseapp.com",
   databaseURL: "https://github-9d1dc-default-rtdb.firebaseio.com",
   projectId: "github-9d1dc",
-  storageBucket: "github-9d1dc.firebasestorage.app",
+  storageBucket: "github-9d1dc.appspot.com",
   messagingSenderId: "1050755592261",
   appId: "1:1050755592261:web:53a86501b3e6cbc558b32c",
   measurementId: "G-7W81L98W1X"
@@ -15,6 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const database = getDatabase(app);
+const db = getDatabase(app);
 
-export { auth, database };
+export { auth, db };
